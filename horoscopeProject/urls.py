246c8c32ed0e498efa295db4a54844f1860aa289
dvornick <mail@dvornick.ru>
2022-10-23 15:18:52 +0300

    Add horoscope URL Configuration

--- horoscopeProject/urls.py
+++ horoscopeProject/urls.py
@@ -14,21 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from horoscope import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('horoscope/aries', views.aries),
-    path('horoscope/taurus', views.taurus),
-    path('horoscope/gemini', views.gemini),
-    path('horoscope/cancer', views.cancer),
-    path('horoscope/leo', views.leo),
-    path('horoscope/virgo', views.virgo),
-    path('horoscope/libra', views.libra),
-    path('horoscope/scorpio', views.scorpio),
-    path('horoscope/sagittarius', views.sagittarius),
-    path('horoscope/capricorn', views.capricorn),
-    path('horoscope/aquarius', views.aquarius),
-    path('horoscope/pisces', views.pisces),
+    path('horoscope/', include('horoscope.urls')),
 ]
